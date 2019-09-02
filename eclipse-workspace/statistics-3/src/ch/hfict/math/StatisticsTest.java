e12b4b4c32c8ec4e0eadcf1fcf118e7234963739
flyer0 <flyer0@github.com>
2019-09-02 20:00:44 +0200

    improve code

--- eclipse-workspace/statistics-3/src/ch/hfict/math/StatisticsTest.java
+++ eclipse-workspace/statistics-3/src/ch/hfict/math/StatisticsTest.java
@@ -2,6 +2,7 @@ package ch.hfict.math;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 
 public class StatisticsTest{
 
@@ -11,7 +12,10 @@ public class StatisticsTest{
         try{
     Statistics s = new StatisticsImpl();
    
-    BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+    BufferedReader r = new BufferedReader(
+            //new InputStreamReader(System.in)
+            new StringReader("1 2 3")); // helps for testing. StringReader
+    
     System.out.print("Eingabe mit Leerzeichen: ");
     s.read(r);
     
