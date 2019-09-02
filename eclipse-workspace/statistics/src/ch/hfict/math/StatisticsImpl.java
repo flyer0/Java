578b83b48adaa275622be5667be8185c6f78da51
flyer0 <flyer0@github.com>
2019-09-02 18:01:19 +0200

    statistics2

---
+++ eclipse-workspace/statistics/src/ch/hfict/math/StatisticsImpl.java
@@ -0,0 +1,39 @@
+package ch.hfict.math;
+import java.util.ArrayList;
+public class StatisticsImpl implements Statistics{
+  
+  ArrayList<Double>numbers=new ArrayList<Double>(); // Arraylist collection definition
+  
+
+
+  public StatisticsImpl(int size) {
+      
+      Double dsize = (double) size; //Cast int to Double
+      
+      //Exceptions -- show exception if size < 0
+      if(size<0) {
+          throw new IllegalArgumentException("size must be > 0");
+      }
+      numbers.add(dsize);
+  }
+
+  @Override
+  public void addNumber(double n) {
+    numbers.add(n);
+  }
+  
+  @Override
+  public double getAverange() {
+      double sum = 0;
+      for(Double e:numbers ) { //Extended For-Loop
+          
+          sum +=e;
+         
+      }
+     return sum/numbers.size();
+
+
+  }
+
+
+}
